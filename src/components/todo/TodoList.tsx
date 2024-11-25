4a3db8168c4165f06b5c079e99731e42570712c6
hjlee3114 <hwajin3114@gmail.com>
2024-11-25 23:28:56 +0900

    할일 목록 : 시간 순 정렬 처리

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -7,10 +7,19 @@ interface TodoListProps {
 }
 
 const TodoList: React.FC<TodoListProps> = ({ todos }) => {
+  console.log(todos);
+
+  const sortedTodos = todos.sort((a, b) => {
+    const timeA = a.time ? a.time.split(":").map(Number) : [Infinity, Infinity];
+    const timeB = b.time ? b.time.split(":").map(Number) : [Infinity, Infinity];
+
+    return timeA[0] === timeB[0] ? timeA[1] - timeB[1] : timeA[0] - timeB[0];
+  });
+
   return (
     <div id="todo-list" className="w-full md:w-96 py-3">
       <div className="min-h-64 bg-yellow-100 rounded-lg p-6">
-        {todos.map((todo) => (
+        {sortedTodos.map((todo) => (
           <TodoItem key={todo.id} todo={todo} />
         ))}
       </div>
