0855d9d36fc1a04a5de495a1c53fb03fa0c0ed29
hjlee3114 <hwajin3114@gmail.com>
2024-11-26 12:23:21 +0900

    main 화면 수정

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -3,12 +3,19 @@ import { PostItButton } from "../components/common/PostItButton";
 
 export const MainPage = () => {
   return (
-    <div className="flex flex-col justify-center items-center text-center">
-      <h1 className="text-2xl font-bold mb-4">✏️ 오늘의 생성</h1>
+    <div className="flex flex-col justify-center items-center text-center min-h-screen">
+      <h1 className="text-3xl font-bold mb-6 text-blue-600">✏️ 오늘도 뚝딱!</h1>
 
-      <div className="flex gap-3 mx-auto my-0">
-        <PostItButton text="투두 리스트" className="bg-green-200" to="/todo" />
-        <PostItButton text="썸네일" className="bg-red-200" />
+      <div className="flex gap-4 mx-auto my-0">
+        <PostItButton
+          text="투두 리스트"
+          className="bg-green-200 hover:bg-green-300 transition duration-300"
+          to="/todo"
+        />
+        <PostItButton
+          text="썸네일"
+          className="bg-red-200 hover:bg-red-300 transition duration-300"
+        />
       </div>
     </div>
   );
